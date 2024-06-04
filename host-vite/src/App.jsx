0c53bd367eb94c679b02a49a3cb910361ba8761c
ggiavatto <ggiavatto@visualsoftware.it>
2024-06-04 09:01:26 +0200

    add new host build with vite

---
+++ host-vite/src/App.jsx
@@ -0,0 +1,13 @@
+import AppR2 from "remote2/AppR2";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>HOST - VITE</h1>
+      <AppR2 />
+    </>
+  );
+}
+
+export default App;
